f7e3e87884dc4041d3d1ec384cd7e33684044613
zakhar.kravtsov <kravtsov.zakhar@gmail.com>
2020-12-01 21:10:37 +0200

    add functions

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,108 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct ClusterStream {
+
+}
+
+impl Read for ClusterStream {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        todo!();
+    }
+}
+
+impl Write for ClusterStream {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        todo!();
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        todo!();
+    }
+}
+
+impl ClusterStream {
+    fn open(cluster: u32) -> Result<Self> {
+        todo!();
+    }
+
+    fn create() -> Result<Self> {
+        todo!();
+    }
+
+    fn close(self) -> Result<()> {
+        todo!();
+    }
+
+    fn content_cluster(&self) -> u32 {
+        todo!();
+    }
+}
 
 struct DirEntry {
-    
+    parrent: Option<u32>,
+}
+
+impl DirEntry {
+    fn file_new(name: &str, content_cluster: u32, parrent_cluster: u32) -> Result<Self> {
+        todo!();
+    }
+
+    fn dir_new(name: &str, content_cluster: u32, parrent_cluster: u32) -> Result<Self> {
+        todo!();
+    }
+
+    fn rename(&mut self, name: &str) -> Result<()> {
+        todo!();
+    }
+
+    fn update(&self) -> Result<()> {
+        todo!();
+    }
+
+    fn is_file(&self) -> bool {
+        todo!();
+    }
+
+    fn is_dir(&self) -> bool {
+        todo!();
+    }
+
+    fn compare(&self, name: &str) -> bool {
+        todo!();
+    }
+
+    fn parrent(&self) -> Option<u32> {
+        todo!();
+    }
+
+    fn cluster(&self) -> u32 {
+        todo!();
+    }
+
+    fn size(&self) -> u32 {
+        todo!();
+    }
 }
 
 struct File {
     dir_entry: DirEntry,
+    stream: ClusterStream,
 }
 
 impl File {
+    fn new(dir_entry: DirEntry) -> Result<Self> {
+        if dir_entry.is_file() {
+            let stream = ClusterStream::open(dir_entry.cluster())?;
+            Ok(Self {
+                dir_entry,
+                stream,
+            })
+        } else {
+            Err(Error::new(ErrorKind::InvalidInput, "Object is not a file"))
+        }
+    }
+
     fn close(self) -> Result<()> {
         todo!();
     }
@@ -36,11 +128,20 @@ impl Write for File {
 }
 
 struct DirIterator {
-
+    stream: ClusterStream,
 }
 
 impl DirIterator {
-    fn next(&mut self) -> Option<Result<DirEntry>> {
+    fn new(cluster: u32) -> Result<Self> {
+        let stream = ClusterStream::open(cluster)?;
+        Ok(Self { stream })
+    }
+}
+
+impl Iterator for DirIterator {
+    type Item = Result<DirEntry>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         todo!();
     }
 }
@@ -50,20 +151,55 @@ struct Dir {
 }
 
 impl Dir {
-    pub fn file_open(&self, file_name: &str) -> Result<File> {
+    fn add(&self, dir_entry: &DirEntry) -> Result<()> {
         todo!();
     }
 
-    pub fn file_create(&self, file_name: &str) -> Result<File> {
-        todo!();
+    fn new(dir_entry: DirEntry) -> Result<Self> {
+        if dir_entry.is_dir() {
+            Ok(Self { dir_entry })
+        } else {
+            Err(Error::new(ErrorKind::InvalidInput, "Object is not a dir"))
+        }
     }
 
-    pub fn dir_open(&self, dir_name: &str) -> Result<Dir> {
-        todo!();
+    fn find(&self, name: &str) -> Result<DirEntry> {
+        for dir_entry in self.iter()? {
+            let entry = dir_entry?;
+            
+            if entry.compare(name) {
+                return Ok(entry);
+            }
+        }
+
+        Err(Error::new(ErrorKind::InvalidInput, "Can't open folder or file"))
     }
 
-    pub fn dir_create(&self, dir_name: &str) -> Result<Dir> {
-        todo!();
+    pub fn file_open(&self, name: &str) -> Result<File> {
+        let dir_entry = self.find(name)?;
+        File::new(dir_entry)
+    }
+
+    pub fn file_create(&self, name: &str) -> Result<File> {
+        let stream = ClusterStream::create()?;
+        let dir_entry = DirEntry::file_new(name, stream.content_cluster(), self.dir_entry.cluster())?;
+        self.add(&dir_entry)?;
+        File::new(dir_entry)
+    }
+
+    pub fn dir_open(&self, name: &str) -> Result<Dir> {
+        let dir_entry = self.find(name)?;
+        Dir::new(dir_entry)
+    }
+
+    pub fn dir_create(&self, name: &str) -> Result<Dir> {
+        let stream = ClusterStream::create()?;
+        //
+        // need to initialize folder
+        //
+        let dir_entry = DirEntry::dir_new(name, stream.content_cluster(), self.dir_entry.cluster())?;
+        self.add(&dir_entry)?;
+        Dir::new(dir_entry)
     }
 
     pub fn remove(&self, name: &str) -> Result<()> {
@@ -71,10 +207,12 @@ impl Dir {
     }
 
     pub fn rename(&self, name: &str, new_name: &str) -> Result<()> {
-        todo!();
+        let mut dir_entry = self.find(name)?;
+        dir_entry.rename(new_name)?;
+        dir_entry.update()
     }
 
-    pub fn iter(&mut self) -> Result<DirIterator> {
-        todo!();
+    pub fn iter(&self) -> Result<DirIterator> {
+        DirIterator::new(self.dir_entry.cluster())
     }
 }
